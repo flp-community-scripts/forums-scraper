bd097fe99cc3a6988c7d0e77a92327e1f5490ae9
fruitybagel <fruitybagel@proton.me>
2024-02-08 22:29:17 +0100

    ?

--- dump.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-  browser = p.chromium.launch(
-      headless=False)  # Set headless=True to run in headless mode
-  page = browser.new_page()
-  page.goto("https://example.com")
-  print(page.title())
-  browser.close()
